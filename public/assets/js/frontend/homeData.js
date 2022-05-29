141c829a15c52bbc7f1d1a67736e1ca0f2c56192
bruce <969597302@qq.com>
2022-05-29 12:38:55 +0800

    home page

--- public/assets/js/frontend/homeData.js
+++ public/assets/js/frontend/homeData.js
@@ -4,7 +4,7 @@ define([], function() {
       imgUrl: '/assets/img/png/robot-product-01.png',
       bgUrl: '/assets/img/png/bg-product-01.jpg',
       title: 'EOD ROBOT',
-      content: 'The EOD robot is special equipment used by the blasting personal'
+      content: 'The EOD robot is special equipment used by the blasting personal The EOD robot is special equipment used by the blasting personal'
     },
     {
       imgUrl: '/assets/img/png/robot-product-02.png',
@@ -56,6 +56,50 @@ define([], function() {
     }
   ]
 
+  const aboutUs = {
+    items: [
+      { 
+        title: 'Vision', 
+        content: 'Manufacturing Machine With Intelligence' 
+      },
+      {
+        title: 'Mission',
+        content: 'Creating a Highland for R&D and Production of Special Robots in China'
+      },
+      {
+        title: 'Concept',
+        content: 'Globalization Innovation Cooperation'
+      }
+    ],
+    banners: [
+      { 
+        title: 'Qianzhi Robotic', 
+        content: 'Hunan Qianzhi Robot Technology Development Co,. Ltd.',
+        imgUrl: '/assets/img/png/home-about-us-01.jpg'
+      },
+      {
+        title: 'Mission',
+        content: 'Creating a Highland for R&D and Production of Special Robots in China',
+        imgUrl: '/assets/img/png/home-about-us-01.jpg'
+      },
+      {
+        title: 'Concept',
+        content: 'Globalization Innovation Cooperation',
+        imgUrl: '/assets/img/png/home-about-us-01.jpg'
+      }
+    ],
+    list: [
+      {
+        title: 'Future Direction',
+        imgUrl: '/assets/img/png/home-about-us-02.jpg'
+      },
+      {
+        title: '"Platform Plus" Mode',
+        imgUrl: '/assets/img/png/home-about-us-03.jpg'
+      }
+    ]
+  }
+
   const partners = [
     { imgUrl: '/assets/img/png/partners-01.jpg' },
     { imgUrl: '/assets/img/png/partners-02.jpg' },
@@ -64,12 +108,35 @@ define([], function() {
     { imgUrl: '/assets/img/png/partners-05.jpg' },
     { imgUrl: '/assets/img/png/partners-06.jpg' },
     { imgUrl: '/assets/img/png/partners-07.jpg' },
-    { imgUrl: '/assets/img/png/partners-08.jpg' },
+    { imgUrl: '/assets/img/png/partners-08.jpg' }
+  ]
+
+  const news = [
+    {
+      imgUrl: '/assets/img/png/news-01.jpg',
+      title: 'The research group of the demonstration zone of near to end.',
+      content: 'As 2019 is near to end, good things continue to happen. From November 22 to 24, the three-days',
+      date: '2019-03-28'
+    },
+    {
+      imgUrl: '/assets/img/png/news-02.jpg',
+      title: '2019 Ab Dhabi Defense Exhibition.',
+      content: 'As 2022 is near to end, good things continue to happen. From November 22 to 24, the three-days',
+      date: '2019-02-18'
+    },
+    {
+      imgUrl: '/assets/img/png/news-03.jpg',
+      title: '2019 Qianzhi Robot Annual conference',
+      content: 'As 2019 is near to end, good things continue to happen. From November 22 to 24, the three-days',
+      date: '2019-01-19'
+    },
   ]
 
   return {
     ourProduct,
-    partners
+    aboutUs,
+    partners,
+    news
   }
 
 });
