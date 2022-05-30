492d619a8566166e68688a3b4d7bb79aa49af84d
bruce <969597302@qq.com>
2022-05-30 23:46:52 +0800

    Split header and footer, Product list page

---
+++ public/assets/js/frontend/productList/productList.js
@@ -0,0 +1,45 @@
+define([
+  'assets/libs/art-template/dist/template.js'
+], function (Template) {
+
+  const listItems =  Array(9).fill({
+    image: 'assets/img/png/robot-product-01.png',
+    title: 'Plant Protection Robot',
+    description: 'The EOD robot is a special equipment used by the blasting personal.'
+  })
+
+  const $elms = {
+    list: $('#productList'),
+    listTemplate: $('#productListTemplate')
+  }
+
+  const productList = {
+    data: {
+      temp: {
+        list: Template.compile($elms.listTemplate.html()),
+      }
+    },
+
+    init() {
+      this.events()
+      this.render()
+
+    },
+
+    render() {
+      const that = productList
+      $elms.list.html(that.data.temp.list({
+        productList: listItems
+      }))
+    },
+
+
+    events() {
+      
+    },
+  };
+
+  return {
+    productList: productList.init(),
+  };
+});
