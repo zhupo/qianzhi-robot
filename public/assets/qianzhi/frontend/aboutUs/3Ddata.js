0b02c777ecac5f432b34f63eb58927ea1bf50681
bruce <969597302@qq.com>
2022-06-09 00:41:48 +0800

    contact s

---
+++ public/assets/qianzhi/frontend/aboutUs/3Ddata.js
@@ -0,0 +1,20 @@
+//自己创建一个数据
+var data = {
+  letv: [
+    {
+      img: "../../../../../assets/qianzhi/frontend/aboutUs/honor-01.jpg",
+    },
+    {
+      img: "../../../../../assets/qianzhi/frontend/aboutUs/honor-02.jpg",
+    },
+    {
+      img: "../../../../../assets/qianzhi/frontend/aboutUs/honor-03.jpg",
+    },
+    {
+      img: "../../../../../assets/qianzhi/frontend/aboutUs/honor-04.jpg",
+    },
+    {
+      img: "../../../../../assets/qianzhi/frontend/aboutUs/honor-05.jpg",
+    },
+  ],
+};
--- public/assets/qianzhi/frontend/aboutUs/aboutUs.js
+++ public/assets/qianzhi/frontend/aboutUs/aboutUs.js
@@ -13,40 +13,6 @@ define(["../../../../../assets/libs/art-template/dist/template.js"], function (
         return false;
       });
 
-      $("#liveSlide").roundabout({
-        startingChild: 1, // 默认的显示第几张图片
-        easing: "easeOutInCirc",
-        duration: 600, // 运动速度
-        autoplay: false, // 自动播放
-        minScale: 0.5,
-        autoplayDuration: 5000,
-        minOpacity: 0.8, //最小的透明度
-        maxOpacity: 1, //最大的透明度
-        reflect: false,
-        startingChild: 0,
-        autoplayInitialDelay: 5000,
-        autoplayPauseOnHover: false,
-        enableDrag: true,
-        clickToFocusCallback: function () {
-          // 点击当前轮播设为焦点回调
-          liveChange();
-        },
-        autoplayCallback: function () {
-          // 自动轮播的回调
-          liveChange();
-        },
-      });
-      var liveChange = function () {
-        var index = $(".roundabout-in-focus").index();
-        $("#dotList")
-          .find("span")
-          .removeClass("active")
-          .eq(index)
-          .addClass("active");
-      };
-      liveChange();
-
-
       var mySwiper = new Swiper ('.swiper', {
         slidesPerView: 3,
         grid: {
